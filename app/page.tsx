19d6b49238a83948491d1d7f28fdee5e350bc09c
winZD <puntica007@gmail.com>
2024-07-11 16:27:05 +0200

    Added logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Camera } from "lucide-react";
 import Image from "next/image";
+import Logo from "../public/logo.svg";
 
 export default function Home() {
   return (
     <main>
       <header className="max-w-6xl px-4 sm:px-8 py-6">
-        <Image width={100} height={100} src={"/vercel.svg"} alt="logo" />
+        <Image src={Logo} alt="logo" />
       </header>
       <section className="max-w-6xl mx-auto sm:px-8 py-6 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
         <h1 className="capitalize text-4xl md:text-7xl font-bold">
