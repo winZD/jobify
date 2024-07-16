270e50da756c5c7c4e0cb584a559c499711c7ead
winZD <puntica007@gmail.com>
2024-07-16 17:22:20 +0200

    added react query and toast

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000 * 5,
+          },
+        },
+      })
+  );
   return (
     <>
       <ThemeProvider
@@ -12,6 +25,11 @@ function Providers({ children }: { children: React.ReactNode }) {
         enableSystem
         disableTransitionOnChange
       >
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
         {children}
       </ThemeProvider>
     </>
