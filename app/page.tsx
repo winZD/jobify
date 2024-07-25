8e6479db9bfe2a33d1c8677414e3c591852afbf9
winZD <puntica007@gmail.com>
2024-07-25 15:28:16 +0200

    Fixing layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ import LandingImg from "../public/business_plan.svg";
 export default function Home() {
   return (
     <main>
-      <header className="max-w-6xl px-4 sm:px-8 py-6">
+      <header className="max-w-6xl px-4 sm:px-8 pt-6">
         <Image src={Logo} alt="logo" />
       </header>
-      <section className="max-w-6xl mx-auto sm:px-8 py-6 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+      <section className="max-w-6xl pt-6 mx-auto px-4 justify-center sm:justify-start sm:px-8  h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
         <div>
           <h1 className="capitalize text-4xl md:text-7xl font-bold">
             job <span className="text-primary">tracking</span> app
